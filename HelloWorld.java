8321b295a352ec401597a458d316177b98dcd2ec
wange <wange@163.com>
2022-04-04 11:18:09 +0800

    冲突分支在hot-fix分支上的修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld{
 	public static void main(String[] args){
 
 		System.out.println("Hello World!");
-		System.out.println("hehe");
+		System.out.println("hahahaha");
+		System.out.println("在hot-fix分支上又添加的内容");
+
 
 	}
 
