7a10e8bd1f1783e941e25b1b2c3052c5a606217f
wange <wange@163.com>
2022-04-04 11:12:24 +0800

    冲突合并在master分支上的修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld{
 	public static void main(String[] args){
 
 		System.out.println("Hello World!");
-		System.out.println("hehe");
+		System.out.println("heihei");
+		System.out.println("在master分支上添加的内容");
 
 	}
 
